50297af75b167f556e2c4e790e51f5b6e2374b3d
Ilya Sanosyan <ilya.sanosyan@awesomeroots.com>
2013-10-28 23:49:45 +0100

    example / test script

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+
+$dir = getcwd();
+chdir('./vendor/tcpdf/examples');
+require_once 'tcpdf_include.php';
+chdir($dir);
+
+function generate_file($i = 0) {
+    $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+    $html = "Hello world!";
+    
+    // Print text using writeHTMLCell()
+    $pdf->AddPage();
+    $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+    $pdf->SetProtection([], 'testpassword', null, 3);
+    $pdf->Output("files/example_$i.pdf", 'F');    
+}
+
+$start = microtime(true);
+do {
+    generate_file(++$count);
+} while (microtime(true) - $start <= 1);
+
+echo $count - 1, "\n";
